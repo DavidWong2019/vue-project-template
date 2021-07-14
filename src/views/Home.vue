43e6839b806003c8fb6ba45e96010f452ad7f55e
huangzhiwei <75641985@qq.com>
2021-07-14 17:13:38 +0800

    新增word导入魔改demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png" />
-    <RichEditorV1 />
+    <RichEditorV2 />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import RichEditorV1 from '@/components/RichEditorV1.vue';
+import RichEditorV2 from '@/components/RichEditorV2.vue';
 
 export default {
   name: 'Home',
   components: {
-    RichEditorV1,
+    RichEditorV2,
   },
 };
 </script>
