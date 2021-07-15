e21d54091764dc3f869a1b9aab2c8d0ad4549e8b
huangzhiwei <75641985@qq.com>
2021-07-15 11:28:54 +0800

    代码提交

---
+++ src/components/rich-editor/SamplePackage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+// 富文本(base on tinymce_V5)
+// ESM版本实现
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/rich-editor/SampleVanilla.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+// 富文本(base on tinymce_V5)
+// 原生js版本实现
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
